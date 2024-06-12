42547b94d5b0f19487f59b8683564c871a90bdce
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-12 18:09:28 -0300

    Refatorado server para usar variáveis de ambiente.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { app } from "./app";
 import bodyParser from 'body-parser';
+
+import { app } from "./app";
 import { env } from "./env";
+
+import router  from "./routes/post.routes";
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true}));
+
+app.use('/api', router)
 
 app.listen(env.PORT, () => {
     console.log(`Server is runnning on port: ${env.PORT}`);
