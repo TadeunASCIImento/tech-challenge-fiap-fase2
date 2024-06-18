7e4d0781c1619139ab5e6f36ff854bad1f2f9d39
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-18 11:31:06 -0300

    Desfazendo alteração equivocada

--- src/http/controllers/posts/find.ts
+++ src/http/controllers/posts/find.ts
@@ -3,14 +3,20 @@ import { z } from "zod";
 import { makeFindPostUseCase } from "../../../use-cases/factory/make-find-post-use-case";
 
 export async function findPostById(request: Request, response: Response) {
-
+    try {
         const findParamSchema = z.object({ 
             id: z.coerce.number() 
         });
-
         const { id } = findParamSchema.parse(request.params);
-        
         const post = await makeFindPostUseCase().handler(id);
         
+        !post ? response.status(203).json('Post not found') : {};
         response.status(200).json(post);
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            response.status(400).json({ errors: error.errors });
+        } else {
+            response.status(500).json({ error: "Internal server error" });
+        }
+    }
 }
