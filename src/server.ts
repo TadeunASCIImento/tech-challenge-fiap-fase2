957ab7e870e0ac11dcc17291b31cdb4ec4eff949
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-18 11:12:46 -0300

    Move a implmentção de app para aap.ts.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import express from "express";
-import bodyParser from 'body-parser';
-
 import { app } from "./app";
 import { env } from "./env";
-
-import postsRouter  from "./routes/post.routes";
-
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true}));
-
-app.use('/api', postsRouter)
 
 app.listen(env.PORT, () => {
     console.log(`Server is runnning on http://localhost:${env.PORT}`);
