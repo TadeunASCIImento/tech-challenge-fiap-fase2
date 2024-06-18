befb0641a62000cf44695394f3b78ffab596569d
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-18 11:07:39 -0300

    Ajustes no código do app.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import express from "express";
+import bodyParser from 'body-parser';
+
+import postsRouter  from "./routes/post.routes";
 
 export const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/api', postsRouter);
