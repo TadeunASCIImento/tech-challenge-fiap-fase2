648eb9c8094df6d6943a44dfd732ae1c19175187
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-20 10:46:08 -0300

    [ORM] Add migration para criação da tabela de usuários.

---
+++ src/lib/orm/migrations/1718888995683-UserCREATE_TABLE.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserCREATETABLE1718888995683 implements MigrationInterface {
+    
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+        `)
+    
+        await queryRunner.query(`
+            CREATE TABLE "users"(
+                ID UUID PRIMARY KEY,
+                USER_NAME VARCHAR(200) NOT NULL,
+                PASSWORD VARCHAR(200) NOT NULL
+            )
+        `)
+
+        await queryRunner.query(`
+            ALTER TABLE users
+            ALTER COLUMN id SET DEFAULT uuid_generate_v4();
+        `)
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE users;
+        `)
+    }
+
+}
