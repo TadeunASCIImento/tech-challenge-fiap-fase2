1ca587e2690bce8c025b54d87a7985b09dc41d0b
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-20 10:45:04 -0300

    Add entidade user

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { IUser } from "./interfaces/user.interface";
+
+@Entity({
+    name:'user'
+})
+export class User implements IUser {
+
+    @PrimaryGeneratedColumn('uuid', {
+        name:'id',
+    })
+    id?: string | undefined;
+
+    @Column({
+        name: 'user_name',
+        type: 'varchar'
+    })
+    username: string;
+
+    @Column({
+        name:'password',
+        type: 'varchar'
+    })
+    password: string;
+
+}
