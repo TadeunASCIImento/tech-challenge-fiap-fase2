60a1821640c04ddbd606d44044449c283b526e4c
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-06-24 09:58:06 -0300

    [AUTH] Adiciona autenticação as rotas de POST, PUT E DELETE e na visão administrativa

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -8,16 +8,17 @@ deletePostById,
 searchPostByKeyword,
 updatePostById  
 } from "../controllers/post/post.controller";
+import { verifyToken } from "../middlewares/jwt/jwt.middleware";
 
 const router = express.Router();
 
-router.post('/api/posts', createPost)
-router.get('/api/posts/admin', findAllPosts)
+router.post('/api/posts', verifyToken, createPost)
+router.get('/api/posts/admin', verifyToken ,findAllPosts)
 router.get('/api/posts', findAllPosts)
 router.get('/api/posts/search', searchPostByKeyword)
 router.get('/api/posts/:id', findPostById)
-router.put('/api/posts/:id', updatePostById)
-router.delete('/api/posts/:id', deletePostById)
+router.put('/api/posts/:id', verifyToken, updatePostById)
+router.delete('/api/posts/:id',verifyToken, deletePostById)
 
 export default router;
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,6 +8,6 @@ from '../controllers/user/user.controller';
 const router = express.Router();
 
 router.post('/api/user', createUser);
-router.post('/api/user/auth', generateToken)
+router.post('/api/user/authorization', generateToken)
 
 export default router;
