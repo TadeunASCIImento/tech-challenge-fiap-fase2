6604353a8b5dd53a8040e14ba41654a9df679350
TadeunASCIImento <tadeunascimento36@gmail.com>
2024-07-02 10:05:31 -0300

    [Doc] Add configs do swagger-ui

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,17 @@ import './lib/orm/typeorm.config'
 import express from "express";
 import bodyParser from 'body-parser';
 
+import swaggerUi from 'swagger-ui-express'; 
+
 import postRouter from "./routes/post.routes";
 import userRouter from "./routes/user.routes";
 
+
 export const app = express();
+
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true}));
